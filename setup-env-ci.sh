5289ac75605b62e3b1aac5d868dd19230ed2e880
Julien Chiquet <julien.chiquet@inrae.fr>
2026-01-21 10:40:24 +0100

    added more system dependencies

--- setup-env-ci.sh
+++ setup-env-ci.sh
@@ -1,1 +1,1 @@
-sudo apt-get install -y libcurl4-openssl-dev libpng-dev chromium cmake libfontconfig1-dev libfreetype6-dev libglpk-dev libjpeg-dev pandoc
+sudo apt-get install -y libcurl4-openssl-dev libpng-dev chromium cmake libfontconfig1-dev libfreetype6-dev libglpk-dev libjpeg-dev pandoc libharfbuzz-dev libfribidi-dev
